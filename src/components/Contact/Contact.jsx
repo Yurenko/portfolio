ed492c7deb454893ca71cbed6359d3be4e97a3f9
Yurenko <didenkoyura225@gmail.com>
2022-07-14 18:39:11 +0300

    Added submit to email

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,34 +1,60 @@
 import { useForm } from 'react-hook-form'
 import styles from './Contact.module.scss'
-
+import emailjs from 'emailjs-com'
+import Swal from 'sweetalert2'
+import { SERVICE_ID, TEMPLATE_ID, USER_ID } from '../../config/constans'
 
 const Contact = () => {
-	const { register, handleSubmit, formState: { errors } } = useForm({
+	const {
+		register,
+		formState: { errors },
+	} = useForm({
 		defaultValues: {
 			name: '',
 			email: '',
-			issues: '',
+			text: '',
 		},
 	})
-	const onSubmit = (data) => console.log(data)
+
+	const onSubmit = (e) => {
+		e.preventDefault()
+		emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID).then(
+			() => {
+				Swal.fire({
+					icon: 'success',
+					title: 'Message Sent Successfully',
+				})
+			},
+			(error) => {
+				console.log(error.text)
+				Swal.fire({
+					icon: 'error',
+					title: 'Ooops, something went wrong',
+					text: error.text,
+				})
+			}
+		)
+		e.target.reset()
+	}
 
 	return (
 		<section className={styles.contact}>
 			<div className="titleInformation">
-				<h2>Contact me</h2>
+				<h1>Contact me</h1>
 				<p className={styles.subtitle}>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga veniam
-					labore nisium illum cupiditate reiciendis a numquam
+					Thank you for your interest for me! <br /> If you have any questions
+					or would like to know more about me, feel free to contact me. I am
+					very approachable!
 				</p>
 			</div>
-			<form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+			<form onSubmit={onSubmit} className={styles.form}>
 				<div className={styles.namePerson}>
 					<input
 						placeholder="Your name"
 						{...register('name', { required: true, maxLength: 15 })}
 					/>
 					{errors.name && <span>This field is required</span>}
-					
+
 					<input
 						placeholder="Your email"
 						type="email"
@@ -37,7 +63,7 @@ const Contact = () => {
 					{errors.email && <span>This field is required</span>}
 				</div>
 
-				<textarea {...register('issues')} placeholder="About you" />
+				<textarea {...register('text')} placeholder="Write me" />
 				{errors.issues && <span>This field is required</span>}
 				<button className={styles.button}>Send message</button>
 			</form>
