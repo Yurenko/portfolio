3336c969d541cf98b922a0c644c6b0305e43f185
Yurenko <didenkoyura225@gmail.com>
2022-07-14 18:42:00 +0300

    Added motion to Stack

--- src/components/Stack/Stack.jsx
+++ src/components/Stack/Stack.jsx
@@ -1,32 +1,30 @@
 import styles from './Stack.module.scss'
+import { motion } from 'framer-motion'
+import data from '../../data.json'
+import { container, item } from '../../config/motionParams'
 
 const Stack = () => {
-	const stackMy = [
-		'react',
-		'redux',
-		'js',
-		'typescript',
-		'css',
-		'api',
-		'git',
-		'github',
-		'vscode',
-		'figma',
-		'photoshop',
-		'sass',
-	]
 	return (
-		<div className={styles.stack}>
-			<h1>My Tech Stack</h1>
-			<p> Technologies I’ve been working with recently</p>
-			<div className={styles.icons}>
-				{stackMy.map((el, index) => (
-					<svg width="120" height="120" key={index}>
-						<use href={`./sprite.svg#${el}`} />
-					</svg>
-				))}
+		<section className={styles.stack}>
+			<div className="titleInformation">
+				<h1>My Tech Stack</h1>
+				<p> Technologies I’ve been working with recently</p>
 			</div>
-		</div>
+			<motion.ul
+				className={styles.icons}
+				variants={container}
+				initial="hidden"
+				whileInView="visible"
+			>
+				{data.stack.map((el, index) => (
+					<motion.li key={index} className="item" variants={item}>
+						<svg width="80" height="80" key={index}>
+							<use href={`./sprite.svg#${el}`} />
+						</svg>
+					</motion.li>
+				))}
+			</motion.ul>
+		</section>
 	)
 }
 
