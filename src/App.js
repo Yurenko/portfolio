6b768e66e71e46035abe33eb32f69b94e47d081b
Yurenko <didenkoyura225@gmail.com>
2022-08-18 15:37:25 +0300

    Changed direction

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Header from './components/Header/Header'
 import { Routes, Route } from 'react-router-dom'
 import MainInformation from './components/MainInformation/MainInformation'
-import Stack from './components/Stack/Stack'
-import Projects from './components/Projects/Projects'
-import Contact from './components/Contact/Contact'
-import About from './components/About/About'
+import Stack from './pages/Stack/Stack'
+import Projects from './pages/Projects/Projects'
+import Contact from './pages/Contact/Contact'
+import About from './pages/About/About'
 
 function App() {
 	return (
