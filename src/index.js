1d1e74a372d27cd5b7a9619376dbc81602b0c4d9
Yurenko <didenkoyura225@gmail.com>
2022-08-30 16:19:33 +0300

    Fix everyting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './globals.scss'
+import './Styles/globals.scss'
 import App from './App'
 
 import { HashRouter } from 'react-router-dom'
